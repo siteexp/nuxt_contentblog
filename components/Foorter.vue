5db196e36054ceec57732e969b989bc67baf3b4f
siteexp <105847409+siteexp@users.noreply.github.com>
2022-07-13 14:13:42 +0200

    Update Foorter.vue

--- components/Foorter.vue
+++ components/Foorter.vue
@@ -1,4 +1,21 @@
+
 <template>
+   <v-toolbar>
+      <v-toolbar-title></v-toolbar-title>
+       <v-toolbar-items class="hidden-sm-and-down">
+        <v-divider
+         class="mx-4"
+         vertical
+        ></v-divider>
+         <v-divider vertical></v-divider>
+         <v-btn
+          color="white"
+          href="/Impressum"
+          text
+         >
+          Impressum
+         </v-btn>
+         
+      </v-toolbar-items>
+    </v-toolbar>
 </template>
-<script>
-</script>
